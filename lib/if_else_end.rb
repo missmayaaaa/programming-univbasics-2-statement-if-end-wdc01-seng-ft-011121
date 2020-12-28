384af8f3b0f81cdbddabb6062a880caf3d54f257
Maya Grant <maya.grant@utexas.edu>
2020-12-28 18:30:45 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,6 +1,10 @@
-# Write your solution here
-if true
-  puts "even!"
+
+
+current_time = Time.now
+current_time = current_time.to_i
+
+if current_time % 2 == 0
+  puts "Even!"
 else
-  puts "odd!"
+  puts "Odd!"
 end
